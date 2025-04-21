1b886db1219914ddf32febde69a6dc87b8a1448e
FrederickKumah <fdblay@gmail.com>
2025-04-21 18:51:15 +0000

    PORT

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.use(express.json());
 
 app.use('/api', propertyRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
