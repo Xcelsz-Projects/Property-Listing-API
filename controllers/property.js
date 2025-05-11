5cde4258b5c6700baa3f186c744d86192c60c55c
FrederickKumah <fdblay@gmail.com>
2025-05-11 13:08:46 +0000

    preparing to host on railway

--- controllers/property.js
+++ controllers/property.js
@@ -1,12 +1,13 @@
-import mysql from 'mysql2/promise';
+import pool from "../utils/db.js";
+// import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 46064,
-});
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT || 46064,
+// });
 
 // POST create new property
 export const createProperty = async (req, res, next) => {
