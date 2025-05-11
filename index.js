c44af9aeb5b1868d94ff3a22053d9d19f6e23188
FrederickKumah <fdblay@gmail.com>
2025-05-11 13:06:22 +0000

    updated index.js

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import helmet from 'helmet';
+// import helmet from 'helmet';
+import pool from './utils/db.js';
 import propertyRouter from './routes/property.js';
 
 const app = express();
-dotenv.config();
 
 
-app.use(helmet());
-app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: ["'self'"],
-            fontSrc: ["'self'", "https://property-listing-api-production.up.railway.app", "data:"],
-            styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles if required
-          },
-    })
-)
+
+// app.use(helmet());
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: ["'self'"],
+//             fontSrc: ["'self'", "https://property-listing-api-production.up.railway.app", "data:"],
+//             styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles if required
+//           },
+//     })
+// )
+
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', propertyRouter);
+
+app.get('/test-db', async (req, res) => {
+    try {
+        const [rows] = await pool.query('SELECT 1 + 1 AS result');
+        
+        res.send(`Database connected: ${rows[0].result}`);
+    } catch (err) {
+        res.status(500).send(`Error: ${err.message}`);
+    }
+});
 
 
 
